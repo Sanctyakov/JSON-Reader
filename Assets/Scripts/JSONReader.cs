0e27a9ebd65726de245b2ee324e4dd57d30dfab5
Sanctyakov <49657655+Sanctyakov@users.noreply.github.com>
2020-07-04 12:29:14 -0300

    Alternative version made without using JsonUtility

--- Assets/Scripts/JSONReader.cs
+++ Assets/Scripts/JSONReader.cs
@@ -4,13 +4,6 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
-[Serializable]
-public struct MainStruct //A serializable struct to store the title and columnheaders, which can be populated via Unity's JsonUtility.
-{
-    public string Title;
-    public string[] ColumnHeaders;
-}
-
 public class JSONReader : MonoBehaviour
 {
     public Text title; //Public variables. References set within the inspector.
@@ -28,23 +21,43 @@ public class JSONReader : MonoBehaviour
     {
         string jsonString = String.Empty;
 
+        //Fake json string used for testing.
+
         /*jsonString = "{\"Title\":\"Team Members\"," +
                      "\"ColumnHeaders\":[\"ID\",\"Name\",\"Role\",\"Nickname\"]," +
                      "\"Data\":[{\"ID\":\"001\",\"Name\":\"John Doe\",\"Role\":\"Engineer\",\"Nickname\":\"KillerJo\",}," +
                      "{\"ID\":\"023\",\"Name\":\"Claire Dawn\",\"Role\":\"Engineer\",\"Nickname\":\"Claw\",}," +
                      "{\"ID\":\"012\",\"Name\":\"Paul Beef\",\"Role\":\"Designer\",\"Nickname\":\"BeefyPaul\",}," +
                      "]" +
-                     "}";*/ //Fake json string used for testing.
+                     "}";*/
 
-        string jsonPath = Path.Combine(Application.streamingAssetsPath, "JsonChallenge.json");
+        string jsonPath = Path.Combine(Application.streamingAssetsPath, "JsonChallenge.json"); //File path is StreamingAssets path.
 
         if (File.Exists(jsonPath))
         {
             jsonString = File.ReadAllText(jsonPath);
 
-            jsonString = RemoveCommasAndWhiteSpaces(jsonString); //A method that cleans up the json string.
+            jsonString = Regex.Replace(jsonString, @"(""[^""\\]*(?:\\.[^""\\]*)*"")|\s+", "$1"); //A regular expression that removes white spaces from everywhere except between quotes.
 
-            PopulateTable(jsonString);
+            title.text = GetFieldContent("Title", jsonString); //The table's title.
+
+            string[] columnHeaders = GetArrayElements(jsonString, ":[\"", "],\"Data\":[{\"", new string[] { "[", "\"", ",", "]" });
+
+            string[] dataStrings = GetArrayElements(jsonString, "\"Data\":[", "]}", new string[] { "{", ",},", "},", "]}", "}" });
+
+            foreach (string columnHeader in columnHeaders)
+            {
+                GameObject newColumn = Instantiate(columnPrefab, columnsTransform); //Columns are fitted within the table's layout group as they are instantiated.
+
+                newColumn.GetComponentInChildren<Text>().text = columnHeader; //Column headers are given names.
+
+                foreach (string dataString in dataStrings)
+                {
+                    GameObject newCell = Instantiate(cellPrefab, newColumn.transform.Find("Cells").transform); //Cells are fitted within the recently instantiated column's layout group.
+
+                    newCell.GetComponentInChildren<Text>().text = GetFieldContent(columnHeader, dataString); //The cell is given its text.
+                }
+            }
         }
         else
         {
@@ -52,58 +65,37 @@ public class JSONReader : MonoBehaviour
         }
     }
 
-    private string RemoveCommasAndWhiteSpaces(string jsonString)
+    private string GetFieldContent(string field, string content) //Locates a field and returns its content.
     {
-        jsonString = Regex.Replace(jsonString, @"(""[^""\\]*(?:\\.[^""\\]*)*"")|\s+", "$1"); //A regular expression that removes white spaces from everywhere except between quotes.
+        field = "\"" + field + "\":\""; //Add symbols for easy removal.
 
-        jsonString = jsonString.Replace(",]", "]"); //Removes commas so JsonUtility can read the string.
-        jsonString = jsonString.Replace(",}", "}");
-
-        return jsonString;
-    }
-
-    private void PopulateTable(string jsonString)
-    {
-        MainStruct mainStruct = JsonUtility.FromJson<MainStruct>(jsonString); //Fill up the struct.
-
-        title.text = mainStruct.Title; //The table's title.
-
-        ColumnsAndCells(mainStruct.ColumnHeaders, jsonString); //Instantiates columns and fills them up with their corresponding cells.
-    }
-
-    private void ColumnsAndCells (string[] columnHeaders, string jsonString)
-    {
-        string dataString = jsonString.Substring(jsonString.IndexOf("\"" + "Data" + "\":[") + 8); //Removes the title and column headers from the string for further splitting.
-
-        string[] dataStrings = dataString.Split(new string[] { "{" , "},", "]}", "}" }, StringSplitOptions.RemoveEmptyEntries); //Splits the data array in as many elemnts as it has, removing brackets.
-
-        foreach (string columnHeader in columnHeaders)
+        if (!content.Contains(field))
         {
-            GameObject newColumn = Instantiate(columnPrefab, columnsTransform); //Columns are fitted within the table's layout group as they are instantiated.
-
-            newColumn.GetComponentInChildren<Text>().text = columnHeader; //Column headers are given names.
-
-            foreach (string dataElement in dataStrings)
-            {
-                string headerWithSymbols = "\"" + columnHeader + "\":\""; //Adds symbols for easy removal.
-
-                string cellText = dataElement; //Once everything that doesn't belong is removed, this string will give the cell its text.
-
-                if (!cellText.Contains(headerWithSymbols))
-                {
-                    cellText = String.Empty; //If the data element does not contain the current header, the cell will be shown empty.
-                }
-                else
-                {
-                    cellText = cellText.Substring(cellText.IndexOf(headerWithSymbols) + headerWithSymbols.Length); //Removes the header and leave the field's content.
-
-                    cellText = cellText.Remove(cellText.IndexOf("\"")); //Removes everything from the field's content onwards.
-                }
-
-                GameObject newCell = Instantiate(cellPrefab, newColumn.transform.Find("Cells").transform); //Cells are fitted within the recently instantiated column's layout group.
-
-                newCell.GetComponentInChildren<Text>().text = cellText; //The cell is given its text.
-            }
+            content = String.Empty; //If the data element does not contain the current header, the cell will be shown empty.
         }
+        else
+        {
+            content = Isolate(content, field, "\"");
+        }
+
+        return content;
+    }
+
+    private string[] GetArrayElements (string anyString, string start, string end, string[] separators) //Locates an array and returns its elements.
+    {
+        anyString = Isolate(anyString, start, end);
+
+        string[] returnArray = anyString.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+        return returnArray;
+    }
+
+    private string Isolate (string anyString, string start, string end) //Isolates a substring from the rest of the string.
+    {
+        anyString = anyString.Substring(anyString.IndexOf(start) + start.Length); //Leaves the start string out.
+
+        anyString = anyString.Remove(anyString.IndexOf(end)); //Removes everything from the end string onwards.
+
+        return anyString;
     }
 }
